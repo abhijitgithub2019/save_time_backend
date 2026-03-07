ec8de82b2220b69f51a3a6b70ee35772f149c090
Abhijit Patra <abhijit.patra224@gmail.com>
2026-03-07 17:31:06 +0530

    Added razor pay for andriod app

--- server.js
+++ server.js
@@ -517,13 +517,10 @@ app.post(
         callback_url ||
         "chrome-extension://hokdmlppdlkokmlolddngkcceadflbke/premium.html",
       callback_method: "get",
-      prefill: {
-        contact: "9999999999",   // auto-filled mobile
-        email: email
-      },    
       options: {
         checkout: {
           name: "BlockSocialMedia", // forces app name in checkout
+          email: email
         },
       },
     };
